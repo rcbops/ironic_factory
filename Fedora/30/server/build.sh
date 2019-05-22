3282711f631b4d3b476caa66dca504526cd35f5a
Cody Bunch <bunchc@gmail.com>
2019-05-22 15:24:20 -0500

    vmware does not like vnc passwords, apparently

--- Fedora/30/server/build.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-TMPDIR=/virtual_machines/tmp/ packer build -except=qemu -var-file=../../../private_vars.json -var-file=box_info.json -var-file=template.json ../../fedora-server.json
-TMPDIR=/virtual_machines/tmp/ packer build -only=qemu -var-file=../../../private_vars.json -var-file=box_info.json -var-file=template.json ../../fedora-server.json
